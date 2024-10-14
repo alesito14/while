ef859380d1b9ca63454594bf8ed103c5f8175e99
Alejandro Rivera <141943838+alesito14@users.noreply.github.com>
2024-10-14 13:06:40 -0600

    Add files via upload

---
+++ while/buclewhile.java
@@ -0,0 +1,11 @@
+public class buclewhile{
+
+    public static void main(String[] args){
+
+        int cont = 0;
+        while (cont <10){
+            System.out.println("estoy dando vueltas while " + cont);
+            cont = cont +1;
+        }
+    }
+}
